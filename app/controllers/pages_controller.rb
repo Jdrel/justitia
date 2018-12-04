1e7867ba641c88758af6900fefbb2c75d871a963
Daniel Mera de Agustin <daniel.meradeagustin@gmail.com>
2018-12-04 16:09:53 +0100

    testing video front end

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :video]
 
   def home
+  end
+
+  def video
   end
 end
