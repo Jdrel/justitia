0a6d1ec38f22670ccbaa4a2e7508f5fe6e9d1fa8
Jascha Drel <jaschadrel@gmail.com>
2018-12-04 15:35:50 +0100

    changed the database to have the duplicate fields in the user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_114017) do
+ActiveRecord::Schema.define(version: 2018_12_04_152156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "clients", force: :cascade do |t|
     t.bigint "user_id"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "address"
     t.string "stripe_token"
     t.integer "stripe_id"
     t.datetime "created_at", null: false
@@ -47,9 +44,6 @@ ActiveRecord::Schema.define(version: 2018_12_04_114017) do
 
   create_table "lawyers", force: :cascade do |t|
     t.bigint "user_id"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "address"
     t.text "description"
     t.integer "years_of_experience"
     t.integer "hourly_rate"
@@ -71,6 +65,9 @@ ActiveRecord::Schema.define(version: 2018_12_04_114017) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
