96dce93cf1dd57622ebbc72ace75fea618731d6a
Jascha Drel <jaschadrel@gmail.com>
2018-12-05 18:49:55 +0100

    added some more styling

--- app/models/lawyer.rb
+++ app/models/lawyer.rb
@@ -2,4 +2,12 @@ class Lawyer < ApplicationRecord
   belongs_to :user
   has_many :consultations, dependent: :destroy
   has_many :specialties, dependent: :destroy
+  def categories
+    categories_names = []
+    self.specialties.each do |specialty|
+      categories_names << specialty.category.name
+    end
+    categories_names_string = categories_names.join(", ")
+    return categories_names_string
+  end
 end
