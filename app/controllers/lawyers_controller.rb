3562c3ab155117ed10a35f38b2e92c8b657005c0
Jascha Drel <jaschadrel@gmail.com>
2018-12-05 16:15:41 +0100

    started with filter on index page & fixed autocomplete

--- app/controllers/lawyers_controller.rb
+++ app/controllers/lawyers_controller.rb
@@ -1,6 +1,7 @@
 class LawyersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
+    get_category_names
     if params[:category] == ""
       @lawyers = Lawyer.all
     else
@@ -17,7 +18,26 @@ class LawyersController < ApplicationController
     end
   end
 
+
+
+  # def search(params)
+
+  #   search_params = []
+
+  # end
+
   def show
     @lawyer = Lawyer.find(params[:id])
   end
+
+  private
+
+  def get_category_names
+    @categories_names = []
+    Category.all.each do |category|
+      @categories_names << category.name
+    end
+    return @categories_names
+  end
+
 end
