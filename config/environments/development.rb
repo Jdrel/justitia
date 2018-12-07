a40d2ab390b54e9b45d389d9396a88a32bc474a6
Jascha Drel <jaschadrel@gmail.com>
2018-12-07 11:29:06 +0100

    added some mailer adjustments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,12 +1,16 @@
 Rails.application.configure do
 
-  # # mail opener
-  config.action_mailer.delivery_method = :letter_opener
+  # # mail opener with LETTER OPENER
+  # config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Postmark Setup
+  config.action_mailer.delivery_method = :postmark
+  config.action_mailer.postmark_settings = { :api_token => ENV['POSTMARK_API_KEY'] }
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
