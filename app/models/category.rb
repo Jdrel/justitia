659c3cf605168446e607bad7e09472f55bb7094d
Jascha Drel <jaschadrel@gmail.com>
2018-12-07 13:07:43 +0100

    made sure the category names  are ordered alphabetically on the homepage

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,14 @@
 class Category < ApplicationRecord
   has_many :specialties
   CATEGORIES = ['Corporate', 'Employment', 'Finance', 'Intellectual Property', 'Family', 'Tax', 'Criminal Defense', 'Traffic', 'Personal Injury', 'Digital Media & Internet', 'Immigration', 'Real Estate', 'Other']
+
+  def get_category_names_alphabetically_ordered
+    @category_names = []
+    Category.all.each do |category|
+      @category_names << category.name
+    end
+    @category_names.sort_by!{ |c| c.downcase }
+    @category_names.insert(0,"All categories")
+    return @category_names
+  end
 end
