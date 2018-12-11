0cae86a7ee44ac3f570ae27d25a3c7658917c097
Daniel Mera de Agustin <daniel.meradeagustin@gmail.com>
2018-12-11 14:51:05 +0100

    fixing bugs

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -8,14 +8,18 @@ class Consultation < ApplicationRecord
     duration / 60
   end
 
+  def duration_to_units
+    minutes = duration_to_minutes
+    (minutes / 5).abs + 1
+  end
+
   def end_time
     start_time + duration
   end
 
   def calculate_client_amount
     rate = lawyer.calculate_5mins_rate
-    minutes = duration_to_minutes
-    five_minutes_block = (minutes / 5).abs
-    (rate * five_minutes_block + rate) * 100
+    five_minutes_units = duration_to_minutes
+    rate * five_minutes_units * 100
   end
 end
