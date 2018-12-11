ef7a4aecd613bca3aa316d984b6118ba2e2b7845
Jascha Drel <jaschadrel@gmail.com>
2018-12-11 14:52:08 +0100

    1. Added some lawyer policies 2. Made sure the date picker works for all dates and the format works 3. made sure a user can create a future appointment

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -3,6 +3,7 @@ class ConsultationsController < ApplicationController
   TW_API_KEY = ENV['TWILIO_KEY']
   TW_API_SECRET = ENV['TWILIO_SECRET']
 
+  # INSTANT CONSULTATIONS
   def index
     @lawyer = Lawyer.find(params[:lawyer_id])
     authorize(@lawyer)
@@ -16,43 +17,41 @@ class ConsultationsController < ApplicationController
   end
 
   def create
-    client = current_user.client
-    if client.stripe_id.nil?
-      customer = Stripe::Customer.create({
-      source: params[:stripeToken],
-      email:  params[:stripeEmail]
-      })
-      client.stripe_id = customer.id
-      client.save
-    end
-    lawyer = Lawyer.find(params[:lawyer_id])
-    consultation = Consultation.new
-    consultation.lawyer = lawyer
-    consultation.client = client
-    consultation.save
-    UserMailer.new_consultation(consultation).deliver_now
-    redirect_to lawyer_consultation_path(lawyer, consultation)
+    set_basic_details_for_new_consultation
+    @consultation.save
+    UserMailer.new_consultation(@consultation).deliver_now
+    redirect_to lawyer_consultation_path(@lawyer, @consultation)
   end
 
-  def appointment_status
-    # if authenticate_lawyer
-      @consultation = Consultation.find(params[:id])
-      @lawyer = @consultation.lawyer
-      @consultation.appointment_status = params[:appointment_status]
-      @consultation.save
-      respond_to do |format|
-        format.js
-      end
-    # else
-    #   redirect_to lawyers_path
-    # end
+  # FUTURE CONSULTATIONS (i.e. appointments)
+  def new_appointment
+    @consultation = Consultation.new
+    @client = current_user.client
   end
 
-  # def authenticate_lawyer
-  #   lawyer = Lawyer.find(params[:lawyer_id])
-  #   current_user.id == lawyer.user.id
-  # end
+  def create_new_appointment
+    set_basic_details_for_new_consultation
+    @consultation.appointment_time = params[:appointment_time]
+    @consultation.appointment_status = "Pending"
+    @consultation.save
+    redirect_to appointment_confirmation_path(@lawyer, @consultation)
+  end
 
+  # SHOW PAGE OF APPOINTMENT CONFIRMATION
+  def appointment_confirmation
+    @consultation = Consultation.find(params[:id])
+    @lawyer = Lawyer.find(params[:lawyer_id])
+  end
+
+  # METHOD THAT ALLOWS THE LAWYER TO CHANGE THE STATUS OF THE APPOINTMENT AND SENDS AN EMAIL  TO BOTH PARTIES
+  def update_appointment_status
+    @consultation = Consultation.find(params[:id])
+    @lawyer = @consultation.lawyer
+    @consultation.appointment_status = params[:appointment_status]
+    @consultation.save
+  end
+
+  # PAGE WHERE CLIENT AND USER HAVE A CALL
   def show
     @consultation = Consultation.find(params[:id])
     authorize(@consultation)
@@ -61,6 +60,8 @@ class ConsultationsController < ApplicationController
     @twilio_token = token.to_jwt
     start_consultation if current_user.lawyer == @consultation.lawyer
   end
+
+  private
 
   def start_consultation
     @consultation.start_time = Time.new if @consultation.start_time.nil?
@@ -75,11 +76,11 @@ class ConsultationsController < ApplicationController
       @consultation.client_amount_cents = calculate_client_amount
 
       charge = Stripe::Charge.create(
-      customer: @consultation.client.stripe_id,
-      amount: @consultation.client_amount_cents,
-      currency: @consultation.client_amount_currency,
-      description: "Consultation: #{@consultation.id}"
-      )
+        customer: @consultation.client.stripe_id,
+        amount: @consultation.client_amount_cents,
+        currency: @consultation.client_amount_currency,
+        description: "Consultation: #{@consultation.id}"
+        )
 
       @consultation.payment_status = 'paid'
       @consultation.client_payment = charge.to_json
@@ -87,39 +88,7 @@ class ConsultationsController < ApplicationController
       @consultation.duration = 0
       @consultation.payment_status = 'cancelled'
     end
-
-    # close the room
-    # @client = Twilio::REST::Client.new(TW_ACCOUNT_SID, twilio_token)
-    # room = @client.video.rooms('DailyStandup').fetch
-    # room = @client.video.rooms('video_room').update(status: 'completed')
     @consultation.save
-  end
-
-  def calculate_client_amount
-    rate = @consultation.lawyer.calculate_5mins_rate
-    minutes = (@consultation.duration / 60)
-    five_minutes_block = (minutes / 5).abs
-    (rate * five_minutes_block + rate) * 100
-  end
-
-  def new_appointment
-    @consultation = Consultation.new
-  end
-
-  def create_new_appointment
-    @consultation = Consultation.new
-    lawyer = Lawyer.find(params[:lawyer_id])
-    @consultation.lawyer = lawyer
-    @consultation.start_time = params[:start_time]
-    @consultation.client = current_user.client
-    @consultation.save
-    # UserMailer.user_consultation(@consultation).deliver_now
-    redirect_to confirmation_path(lawyer, @consultation)
-  end
-
-  def confirmation
-    @consultation = Consultation.find(params[:id])
-    @lawyer = Lawyer.find(params[:lawyer_id])
   end
 
   def twilio_token
@@ -135,6 +104,30 @@ class ConsultationsController < ApplicationController
       TW_API_SECRET,
       [video_grant],
       identity: identity
-    )
+      )
+  end
+
+  def set_basic_details_for_new_consultation
+    # Setting the Stripe details, lawyer and creating the consultation.
+    client = current_user.client
+    if client.stripe_id.nil?
+      customer = Stripe::Customer.create({
+        source: params[:stripeToken],
+        email:  params[:stripeEmail]
+      })
+      client.stripe_id = customer.id
+      client.save
+    end
+    @lawyer = Lawyer.find(params[:lawyer_id])
+    @consultation = Consultation.new
+    @consultation.lawyer = @lawyer
+    @consultation.client = client
+  end
+
+  def calculate_client_amount
+    rate = @consultation.lawyer.calculate_5mins_rate
+    minutes = (@consultation.duration / 60)
+    five_minutes_block = (minutes / 5).abs
+    (rate * five_minutes_block + rate) * 100
   end
 end
---
+++ app/policies/lawyer_policy.rb
@@ -0,0 +1,11 @@
+class LawyerPolicy < ApplicationPolicy
+  def index?
+    user == record.user
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
